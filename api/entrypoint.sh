3a12c4ec0c26dd81688833136658e804f6ed2736
Julie Sullivan <julie.sullivan@gmail.com>
2019-11-06 10:53:40 +0000

    update with cellbase perl scripts

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e 
 
-PERL5LIB=${PERL5LIB}:/opt/bioperl-1.6.1
+PERL5LIB=${PERL5LIB}:/opt/bioperl-live
 PERL5LIB=${PERL5LIB}:/opt/ensembl/modules
 PERL5LIB=${PERL5LIB}:/opt/ensembl-compara/modules
 PERL5LIB=${PERL5LIB}:/opt/ensembl-variation/modules
 PERL5LIB=${PERL5LIB}:/opt/ensembl-funcgen/modules
-PERL5LIB=${PERL5LIB}:/opt/ensembl-io/modules
 PERL5LIB=${PERL5LIB}:/opt/lib/perl/5.18.2/
-PERL5LIB=${PERL5LIB}:/opt/ensembl-rest/modules
-
+PERL5LIB=${PERL5LIB}:/opt/
 export PERL5LIB
 
 PATH=${PATH}:/opt/tabix/
